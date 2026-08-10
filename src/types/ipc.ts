b579c43bf384904fa2e0e3bee0ab7a0560ad3714
Thoriso04 <thorisombambisa004@gmail.com>
2026-08-10 17:46:08 +0200

    feat(types): add IPC channels enum definition

---
+++ src/types/ipc.ts
@@ -0,0 +1,8 @@
+export enum IPC_CHANNELS {
+  AUTH_LOGIN = 'auth:login',
+  PATIENT_GET_BY_ID = 'patient:get-by-id',
+  PATIENT_SAVE = 'patient:save-record',
+  AUDIT_LOG_EVENT = 'audit:log-event',
+  CSV_IMPORT_CAMPERS = 'csv:import-campers',
+  SESSION_LOCK = 'session:lock'
+}
